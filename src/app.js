4bc061066a7577f341e5fe6fb9962d47bc2b4dc7
fresh5447 <doug@kosmojo.com>
2016-01-02 14:51:03 -0700

    base app setup

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
-import app from 'ampersand-app'
-import Router from './router'
-import Me from './models/me'
+import React from 'react'
+import styles from './styles/main.styl'
 
-// webpack lets us include styles like this
-require('./styles/main.styl')
-require('octicons/octicons/octicons.css')
-
-window.app = app.extend({
-  init () {
-    this.me = new Me()
-    this.me.fetchInitialData()
-    this.router = new Router()
-    this.router.history.start()
+const Hello = React.createClass({
+  render (){
+    return <div> Hello, {this.props.name} </div>
   }
 })
 
-app.init()
+React.render(<Hello name="douglas"/>, document.body)
